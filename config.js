e99d143e6abb0717f678c30faa802bd4ce3bf4e6
uvindugit <uvinduf249@gmail.com>
2024-11-19 19:42:51 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'DnIkRaKS#KnYQjzk_CNzOteHUKuzqdSCFsGG-d2KSvQ1nKq8qdBI', // Enter Your Session ID
+    SESSION_ID: process.env.SESSION_ID || 'yjZimDQC#-xIXW7ApKwPsegrbCg4d9PRn82UrbqC5jafoUCXYzhQ', // Enter Your Session ID
     MONGODB: process.env.MONGODB || 'mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/',// Enter Your MongoDB URL
     OWNER_NUMBER: process.env.OWNER_NUMBER || '94760105256',    // Enter Your Owner Number
 };
